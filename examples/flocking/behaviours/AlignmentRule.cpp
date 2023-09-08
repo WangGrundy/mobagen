6a477c267ab0ddd99c2a1a0b664e7456e0a5d0b9
Wang Grundy <2101005@uad.ac.uk>
2023-09-07 23:00:20 -0400

    got alignment working but not cohesion

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -8,5 +8,39 @@ Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boi
   // todo: add your code here to align each boid in a neighborhood
   // hint: iterate over the neighborhood
 
+  bool CheckIfZero(const float& value);
+  bool CheckIfZero(const std::vector<Boid*>& neighborhood);
+
+  float x = 0;
+  float y = 0;
+
+  for (auto* i : neighborhood){
+    x += i->getVelocity().x;
+    y += i->getVelocity().y;
+  }
+
+  if(CheckIfZero(neighborhood)){ //don't average velocity if neighbourhood is 0
+    return averageVelocity;
+  }
+
+  if(!CheckIfZero(x)){ //don't average velocity if velocity is zero
+    x /= neighborhood.size();
+  }
+  if(!CheckIfZero(y)){
+    y /= neighborhood.size();
+  }
+
+  //set velocity
+  averageVelocity.x = x;
+  averageVelocity.y = y;
+
   return Vector2f::normalized(averageVelocity);
+}
+
+bool CheckIfZero(const float& value){
+  return value == 0;
+}
+
+bool CheckIfZero(const std::vector<Boid*>& neighborhood){
+  return neighborhood.empty();
 }
--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -8,6 +8,35 @@ Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid
   // hint: iterate over the neighborhood
 
   // find center of mass
+  bool CheckIfZero(const float& value);
+  bool CheckIfZero(const std::vector<Boid*>& neighborhood);
+
+  float xPos = 0;
+  float yPos = 0;
+
+  for(auto* i : neighborhood){ //Add all positions
+    xPos += i->getPosition().x;
+    xPos += i->getPosition().y;
+  }
+
+    if(CheckIfZero(neighborhood)){ //don't average cohesion if neighbourhood is 0
+      return cohesionForce;
+    }
+
+    if(!CheckIfZero(xPos)){ //don't average cohesion if position is zero
+      xPos /= neighborhood.size();
+    }
+    if(!CheckIfZero(yPos)){
+      yPos /= neighborhood.size();
+    }
 
   return cohesionForce;
+}
+
+bool CheckIfZero(const float& value){
+  return value == 0;
+}
+
+bool CheckIfZero(const std::vector<Boid*>& neighborhood){
+  return neighborhood.empty();
 }
