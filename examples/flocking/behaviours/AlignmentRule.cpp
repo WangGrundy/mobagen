a383c9fa462b717b07ab18fc6a32a1ba74ef2b11
Wang Grundy <2101005@uad.ac.uk>
2023-09-10 11:04:43 -0400

    doing separation, no idea whats happening

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -9,19 +9,22 @@ Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boi
   // todo: add your code here to align each boid in a neighborhood
   // hint: iterate over the neighborhood
 
+  //check if neighbour hood is zero
+  if(AdditionalFunctions::CheckIfZero(neighborhood)){ //don't average velocity if neighbourhood is 0
+    return boid->getVelocity();
+  }
+
   //iterate over the neighbourhood and add each velicity
   for (auto* i : neighborhood){
     averageVelocity += i->getVelocity();
   }
 
-  //check if neighbour hood is zero
-  if(AdditionalFunctions::CheckIfZero(neighborhood)){ //don't average velocity if neighbourhood is 0
-    return averageVelocity;
-  }
-
-  //check if neighbour hood velocity is zero and then divide to get average velocity
+  //check if neighbourhood velocity is zero and then divide to get average velocity
   if(!AdditionalFunctions::CheckIfZero(averageVelocity)){ //don't average velocity if velocity is zero
     averageVelocity /= neighborhood.size();
+  }
+  else{
+    return boid->getVelocity();
   }
 
   return Vector2f::normalized(averageVelocity);
--- examples/flocking/behaviours/SeparationRule.cpp
+++ examples/flocking/behaviours/SeparationRule.cpp
@@ -3,44 +3,59 @@
 #include "../gameobjects/World.h"
 #include "engine/Engine.h"
 #include "AdditionalFunctions.h"
+#include <limits>
+#include <cmath>
 
 Vector2f SeparationRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   // Try to avoid boids too close
   Vector2f separatingForce = Vector2f::zero();
 
-  float desiredDistance = desiredMinimalDistance;
-  float x = 0;
-  float y = 0;
-
   // todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
-//  if (!neighborhood.empty()) {
-//      Vector2f position = boid->transform.position;
-//      int countCloseFlockmates = 0;
-//      // todo: find and apply force only on the closest mates
-//  }
+  //  if (!neighborhood.empty()) {
+  //      Vector2f position = boid->transform.position;
+  //      int countCloseFlockmates = 0;
+  //      // todo: find and apply force only on the closest mates
+  //  }
 
-  for(auto* i : neighborhood){ //Add all positions
-    x += i->getPosition().x;
-    y += i->getPosition().y;
-  }
+  float desiredDistance = desiredMinimalDistance;
+  Vector2f ourPosition = Vector2f::zero();
+  float lowestDistance= std::numeric_limits<float>::max();
+  float currentDistance = -1;
+  Vector2f closestFlockMate(0,0);
 
+  //check if neighborhood is zero
   if(AdditionalFunctions::CheckIfZero(neighborhood)){ //don't average velocity if neighbourhood is 0
+    separatingForce = Vector2f::normalized(separatingForce);
     return separatingForce;
   }
 
-  if(!AdditionalFunctions::CheckIfZero(x)){ //don't average velocity if velocity is zero
-    x /= neighborhood.size();
-  }
-  if(!AdditionalFunctions::CheckIfZero(y)){
-    y /= neighborhood.size();
+  ourPosition = boid->transform.position;
+
+  for(auto* i : neighborhood){ //loop through all boids near us to find closest
+
+    //distance = √((x2 – x1)² + (y2 – y1)²).
+    //track current distance
+    float x = pow((i->getPosition().x - ourPosition.x),2); // (x2 – x1)²
+    float y = pow((i->getPosition().y - ourPosition.y), 2); // (y2 – y1)²
+    currentDistance = sqrt(x + y); // √((x2 – x1)² + (y2 – y1)²).
+
+    //if distance is lower than our lowest, then update it
+    if(currentDistance < lowestDistance){
+      lowestDistance = currentDistance; //Get the closest flock mate position data
+      closestFlockMate.x = i->getPosition().x - ourPosition.x;
+      closestFlockMate.y = i->getPosition().y - ourPosition.y;
+    }
   }
 
-  //set SF
-  separatingForce.x = -x;
-  separatingForce.y = -y;
+  //if distance is less than desiredDistance, move it away inversely proportional to the distance between them
+  if(lowestDistance < desiredDistance){
+    closestFlockMate.x = 1/closestFlockMate.x;
+    closestFlockMate.y = 1/closestFlockMate.y;
+
+    separatingForce = closestFlockMate;
+  }
 
   separatingForce = Vector2f::normalized(separatingForce);
-
   return separatingForce;
 }
 
